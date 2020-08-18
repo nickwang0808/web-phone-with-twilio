13ab6d6b98c4a8fc92d9a0cbfa00e800c06aed9e
Nick Wang <wang703003515@gmail.com>
2020-08-18 12:16:00 +0800

    firt commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// this is for Voip setup, migrate back to main code when finished
+
+import React, { useState, useEffect } from "react";
+const { Device } = require("twilio-client");
 
 function App() {
+  const [deviceReady, setDeviceReady] = useState(false);
+  const [connection, setConnection] = useState(false);
+  const [number, setNumber] = useState("");
+
+  const updateNumber = (event) => {
+    setNumber(event.target.value);
+  };
+
+  const connectCustomer = () => {
+    const params = { number: number };
+    Device.connect(params);
+  };
+
+  const getToken = async () => {
+    const response = await fetch("http://localhost:3000/token/generate", {
+      method: "POST",
+    }).then((body) => body.json());
+
+    console.log(response.token);
+    Device.setup(response.token);
+  };
+
+  useEffect(() => {
+    getToken();
+    Device.on("ready", () => {
+      console.log("device ready");
+      setDeviceReady(true);
+    });
+
+    Device.on("error", (error) => console.log(error));
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div>
+        <button onClick={getToken}>Setup</button>
+        <h3>{deviceReady ? "Device Ready" : "Device not Ready"}</h3>
+        <input name="phoneNumber" onChange={updateNumber} value={number} />
+        <button onClick={connectCustomer}>Call</button>
+        <span>Call Status </span>
+        <span>{connection ? "calling" : "ready"}</span>
+      </div>
+    </React.Fragment>
   );
 }
 
