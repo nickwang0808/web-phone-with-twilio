69b334792a1c94b9234840dfc0aed3c97afb48eb
Nick Wang <wang703003515@gmail.com>
2020-09-12 11:13:34 +0800

    get everything to gcp

--- server/config.js
+++ server/config.js
@@ -1,21 +1,13 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./autodialer-285913-0d8eb486fa4d.json");
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAFiw1oa4U8y8fmABEt6wl6GB497X0uK0E",
-  authDomain: "autodialer-285913.firebaseapp.com",
-  databaseURL: "https://autodialer-285913.firebaseio.com",
-  projectId: "autodialer-285913",
-  storageBucket: "autodialer-285913.appspot.com",
-  messagingSenderId: "954133383749",
-  appId: "1:954133383749:web:b89bec6278fb7966bb712c",
-  measurementId: "G-ZKBT2Q7B3T",
-};
+const serviceAccount = require("./autodialer-285913-firebase-adminsdk-vk2kb-a26ac625f7.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://autodialer-285913.firebaseio.com",
 });
 
 const db = admin.firestore();
+const timeStamp = admin.firestore.FieldValue.serverTimestamp;
 
-module.exports.db = db;
+module.exports = db;
+module.exports.timeStamp = timeStamp;
--- server/routes/message.js
+++ server/routes/message.js
@@ -1,9 +1,35 @@
 const express = require("express");
 const MessagingResponse = require("twilio").twiml.MessagingResponse;
+const db = require("../config").db;
+const timeStamp = require("../config").timeStamp;
 
 const router = express.Router();
 
+// async function addMessage(db, content) {
+//   try {
+//     const docRef = db.collection("messages").doc("test");
+//     await docRef.set({
+//       messageBody: content.body.Body,
+//     });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+
+async function addMessage(db, content) {
+  try {
+    const docRef = db.collection("messages").doc("test");
+    await docRef.set({
+      messageBody: "testtest",
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
 router.post("/sms", (req, res) => {
+  // add date to db
+  addMessage(db, req);
   console.log(req.body.Body);
 });
 
