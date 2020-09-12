dba5a19e33ecf48a4be8e45ed08162afcd4c0da9
Nick Wang <wang703003515@gmail.com>
2020-09-12 14:59:47 +0800

    save text to database

--- server/routes/message.js
+++ server/routes/message.js
@@ -1,26 +1,15 @@
 const express = require("express");
-const MessagingResponse = require("twilio").twiml.MessagingResponse;
 const db = require("../config").db;
 const timeStamp = require("../config").timeStamp;
 
 const router = express.Router();
 
-// async function addMessage(db, content) {
-//   try {
-//     const docRef = db.collection("messages").doc("test");
-//     await docRef.set({
-//       messageBody: content.body.Body,
-//     });
-//   } catch (err) {
-//     console.log(err);
-//   }
-// }
-
-async function addMessage(db, content) {
+async function addMessageToDB(db, content) {
   try {
-    const docRef = db.collection("messages").doc("test");
+    const docRef = db.collection("messages").doc(content.body.From);
     await docRef.set({
-      messageBody: "testtest",
+      messageBody: content.body.Body,
+      timeStamp: timeStamp(),
     });
   } catch (err) {
     console.log(err);
@@ -28,9 +17,9 @@ async function addMessage(db, content) {
 }
 
 router.post("/sms", (req, res) => {
-  // add date to db
-  addMessage(db, req);
+  addMessageToDB(db, req);
   console.log(req.body.Body);
+  res.sendStatus(200);
 });
 
 module.exports = router;
